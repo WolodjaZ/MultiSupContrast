0c4108427043a0cfc37c7df5cd9cf713bced3c98
Vladimir Zaigrajew <vladimirzaigrajew@gmail.com>
2022-07-01 20:13:20 +0200

    Added slurm scripts

---
+++ scripts/main_contr_celeba.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=mutlisupcontrast_contr
+#SBATCH --partition=tesla
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --time=25:00:00
+#SBATCH --mail-user=241368@student.pwr.edu.pl
+
+master_node=${SLURM_NODELIST:0:9}${SLURM_NODELIST:10:4}
+dist_url="tcp://"
+dist_url+=$master_node
+dist_url+=:40000
+
+DATASET_PATH="/home/wolodja5/celeba"
+EXPERIMENT_PATH="/home/wolodja5/results/experiments"
+mkdir -p $EXPERIMENT_PATH
+RUN_ID=0
+
+srun --output=${EXPERIMENT_PATH}/%j.out --error=${EXPERIMENT_PATH}/%j.err --label singularity run --nv pytorch.sif \
+    python -m  torch.distributed.launch --nproc_per_node=1 tmp/MultiSupContrast/main_contr.py \
+    --data=$DATASET_PATH \
+    --data-name=CELEBA \
+    --model-name=tresnet_s \
+    --num-classes=40 \
+    --image-size=64 \
+    --method=MultiSupCon \
+    --temp=0.1 \
+    --feat-dim=128 \
+    --c_treshold=0.2 \
+    --learning_rate=0.25 \
+    --lr_decay_epochs="70,80,90" \
+    --cosine \
+    --batch-size=128 \
+    --epochs=100 \
+    --sync_bn=False \
+    --workers=4 \
+    --dump_path=$EXPERIMENT_PATH
+    --run=$RUN_ID
---
+++ scripts/main_decoder_celeba.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=mutlisupcontrast_decoder
+#SBATCH --partition=tesla
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --time=25:00:00
+#SBATCH --mail-user=241368@student.pwr.edu.pl
+
+master_node=${SLURM_NODELIST:0:9}${SLURM_NODELIST:10:4}
+dist_url="tcp://"
+dist_url+=$master_node
+dist_url+=:40000
+
+DATASET_PATH="/home/wolodja5/celeba"
+EXPERIMENT_PATH="/home/wolodja5/results/experiments_decoder"
+MODEL_PATH="/home/wolodja5/results/experiments/run_0/best_checkpoint.pth.tar"
+mkdir -p $EXPERIMENT_PATH
+RUN_ID=0
+
+srun --output=${EXPERIMENT_PATH}/%j.out --error=${EXPERIMENT_PATH}/%j.err --label singularity run --nv pytorch.sif \
+    python -m  torch.distributed.launch --nproc_per_node=1 tmp/MultiSupContrast/main_decoder.py \
+    --data=$DATASET_PATH \
+    --data-name=CELEBA \
+    --model-name=tresnet_s \
+    --model-path=$MODEL_PATH \
+    --num-classes=40 \
+    --image-size=64 \
+    --learning_rate=1 \
+    --method=Asymetric
+    --freeze=True
+    --batch-size=256 \
+    --epochs=100 \
+    --sync_bn=False \
+    --feat-dim=128 \
+    --workers=4 \
+    --method_used=MultiSupCon \
+    --dump_path=$EXPERIMENT_PATH
+    --run=$RUN_ID
